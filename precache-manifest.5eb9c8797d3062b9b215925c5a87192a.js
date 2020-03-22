d5b6a50c0845dbd921d0fc3f4a8776d70a3ef076
jay-acosta <jayacosta@utexas.edu>
2020-03-21 21:33:10 -0500

    Updates

---
+++ precache-manifest.5eb9c8797d3062b9b215925c5a87192a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5c1de7f685c2dc8484de5fcfac3b2bd",
+    "url": "/othello-react/index.html"
+  },
+  {
+    "revision": "dc878523bc033bbeddb5",
+    "url": "/othello-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b3a88e13e5ec0e871316",
+    "url": "/othello-react/static/js/2.27abe9e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/othello-react/static/js/2.27abe9e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc878523bc033bbeddb5",
+    "url": "/othello-react/static/js/main.e4c319f1.chunk.js"
+  },
+  {
+    "revision": "588502fb1b44c0341b99",
+    "url": "/othello-react/static/js/runtime-main.29c1ec88.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/othello-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
